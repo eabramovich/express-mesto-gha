237ee37e2380b8f428a99f4cc8dd385a0e6d91bc
Evgeniya Abramovich <ewg.abramovich2014@yandex.ru>
2023-11-22 14:18:00 +0300

    Fix bug with import

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express, { json } from 'express';
 import mongoose from 'mongoose';
-import router from './routes';
+import router from './routes/index.js';
 
 const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
