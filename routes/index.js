f18a85edfd32631d19f55c89c3122edd9bfe2c46
Evgeniya Abramovich <ewg.abramovich2014@yandex.ru>
2023-11-22 15:00:09 +0300

    Add router for other pathes

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,7 @@ const router = Router();
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
-
+router.use('*', (req, res) => {
+  return res.status(404).send({ message: 'Ресурс не найден. Проверьте URL и метод запроса'});
+});
 export default router;
