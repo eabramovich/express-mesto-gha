f0308d0811aa1a5f36d3dbdd4eef5be307038dc9
Evgeniya Abramovich <ewg.abramovich2014@yandex.ru>
2023-12-03 01:01:26 +0300

    Added user verification

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import { getUsers, getUserById, createUser, updateUser, updateUserAvatar } from '../controllers/user.js';
+import { Joi, celebrate } from 'celebrate';
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
-userRouter.get('/:userId', getUserById);
+userRouter.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum.length(24),
+  }),
+}),getUserById);
 userRouter.patch('/me', updateUser);
 userRouter.patch('/me/avatar', updateUserAvatar);
 
