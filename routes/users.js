157e1ac6abdfbbd4f32a343bc225290ee404611d
Evgeniya Abramovich <ewg.abramovich2014@yandex.ru>
2023-12-03 01:04:37 +0300

    Added user verification

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const userRouter = Router();
 userRouter.get('/', getUsers);
 userRouter.get('/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum.length(24),
+    userId: Joi.string().alphanum().length(24),
   }),
 }),getUserById);
 userRouter.patch('/me', updateUser);
