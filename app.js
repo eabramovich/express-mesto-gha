70928a4603a41b7d6a74420d4d7726c0fcda8804
Evgeniya Abramovich <ewg.abramovich2014@yandex.ru>
2023-12-03 00:46:54 +0300

    Added user create verification

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.post("/signup", celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string.pattern(new RegExp(urlPattern)),
+    avatar: Joi.string().pattern(new RegExp(urlPattern)),
   }),
 }),createUser);
 app.use(router);
